a3cc8784ffa037e505d6f0110c796c0c8dfb2785
Pruthvisg <pruthvisg28@gmail.com>
2023-01-12 12:17:35 +0530

    Initial Commit version

---
+++ CalculatorLib.Tests/CalculatorTest.cs
@@ -0,0 +1,83 @@
+﻿using NUnit;
+using NUnit.Framework;
+using System;
+using CalculatorLib;
+
+namespace CalculatorLib.Tests
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [TestCase]
+        public void Add_When_Both_Inputs_GreaterThanZero_Returns_ExpectedResult()
+        {
+            // Arrange
+            Calculator calculator= new Calculator();
+
+            int firstNum = 30;
+            int secondNum = 20;
+            int expectedResult = 50;
+            int actualResult;
+
+            // Act
+            actualResult = calculator.Add(firstNum, secondNum);
+
+            // Assert
+            Assert.That(expectedResult == actualResult, "Verify the add method logic");
+        }
+
+        [TestCase]
+        public void Add_When_FirstNum_Greater_Than_Zero_and_SecondNum_LessThanOrEqualTo_Zer0()
+        {
+            // Arrange
+            Calculator calculator = new Calculator();
+
+            int firstNum = 30;
+            int secondNum = -20;
+            int expectedResult = -1;
+            int actualResult;
+
+            // Act
+            actualResult = calculator.Add(firstNum, secondNum);
+
+            // Assert
+            Assert.That(expectedResult == actualResult, "Verify the add method logic");
+        }
+
+        [TestCase]
+        public void Add_When_firstNum_IsLessThanOrEqualTo_Zero_and_secondNum_Greater_Than_Zero()
+        {
+            // Arrange
+            Calculator calculator = new Calculator();
+
+            int firstNum = -30;
+            int secondNum = 20;
+            int expectedResult = -2;
+            int actualResult;
+
+            // Act
+            actualResult = calculator.Add(firstNum, secondNum);
+
+            // Assert
+            Assert.That(expectedResult == actualResult, "Verify the add method logic");
+        }
+
+        [TestCase]
+        public void Add_When_Both_Inputs_LessThan_Zero()
+        {
+            // Arrange
+            Calculator calculator = new Calculator();
+
+            int firstNum = -30;
+            int secondNum = -20;
+            int expectedResult = 0;
+            int actualResult;
+
+            // Act
+            actualResult = calculator.Add(firstNum, secondNum);
+
+            // Assert
+            Assert.That(expectedResult == actualResult, "Verify the add method logic");
+        }
+    }
+}
---
+++ CalculatorLib.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculatorLib.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorLib.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1010d72f-cd2a-4f16-a56a-464c8669d972")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
